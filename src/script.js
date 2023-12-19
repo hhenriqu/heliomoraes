8351618df3a33c35237faca41a2161542811e600
Helio Henrique Moraes <heliohenriquemoraes@gmail.com>
2023-12-19 17:47:21 -0300

    feat: primeira dobra

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 let idiomaAtual = 'pt-br'; // Idioma padrão
+let isDarkMode = false; // Verifica se está no modo escuro
 
 function trocarIdioma(lang) {
   idiomaAtual = lang;
   exibirTraducao();
+  atualizarTextos(); // Atualiza os textos quando o idioma é alterado
 }
 
 function exibirTraducao() {
   const traducao = translations[idiomaAtual];
   const elementos = document.querySelectorAll('[data-translate]');
-  
+
   elementos.forEach(elemento => {
     const chave = elemento.getAttribute('data-translate');
     if (traducao[chave]) {
@@ -20,13 +22,26 @@ function exibirTraducao() {
 // Exibir a tradução inicial
 exibirTraducao();
 
+let textos = []; // Array de textos inicial vazio
 
-const textos = [
-  "Desenvolvedor Front-End",
-  "Desenvolvedor Back-Back",
-  "Desenvolvedor Full-Stack",
-  "Engenheiro de Software"
-];
+function atualizarTextos() {
+  textos =
+    idiomaAtual === 'pt-br'
+      ? [
+          'Desenvolvedor Front-End',
+          'Desenvolvedor Back-End',
+          'Desenvolvedor Full-Stack',
+          'Engenheiro de Software'
+        ]
+      : [
+          'Front-End Developer',
+          'Back-End Developer',
+          'Full-Stack Developer',
+          'Software Engineering'
+        ];
+}
+
+atualizarTextos(); // Atualiza os textos iniciais
 
 let index = 0;
 let isApagando = false;
@@ -34,27 +49,40 @@ const velocidadeDigitacao = 100; // Velocidade em milissegundos
 const tempoApagar = 1000; // Tempo de espera antes de apagar
 
 function mostrarTexto() {
-  const textoAtual = textos[index];
+  const textoAtual = textos[index].toUpperCase();
   const textoElemento = document.getElementById('texto-digitado');
 
   if (!isApagando) {
-    textoElemento.textContent = textoAtual.substring(0, textoElemento.textContent.length + 1);
+    textoElemento.textContent = textoAtual.substring(
+      0,
+      textoElemento.textContent.length + 1
+    );
     if (textoElemento.textContent === textoAtual) {
       isApagando = true;
       setTimeout(mostrarTexto, tempoApagar);
       return;
     }
   } else {
-    textoElemento.textContent = textoAtual.substring(0, textoElemento.textContent.length - 1);
+    textoElemento.textContent = textoAtual.substring(
+      0,
+      textoElemento.textContent.length - 1
+    );
     if (textoElemento.textContent === '') {
       isApagando = false;
       index = (index + 1) % textos.length;
     }
   }
 
-  setTimeout(mostrarTexto, isApagando ? velocidadeDigitacao / 2 : velocidadeDigitacao);
+  setTimeout(
+    mostrarTexto,
+    isApagando ? velocidadeDigitacao / 2 : velocidadeDigitacao
+  );
 }
 
 // Chame a função para iniciar o efeito
 mostrarTexto();
 
+function toggleTheme() {
+  isDarkMode = !isDarkMode;
+  document.body.classList.toggle('dark-mode', isDarkMode);
+}
--- src/translations.js
+++ src/translations.js
@@ -1,18 +1,26 @@
 const translations = {
   'pt-br': {
-    'titulo': 'Página Inicial',
-    'texto': 'Bem-vindo à página inicial em português!',
+    titulo: 'Página Inicial',
+    texto: 'Bem-vindo à página inicial em português!',
     'menu-pagina-inicial': 'Página inicial',
     'menu-sobre': 'Sobre mim',
     'menu-projetos': 'Projetos',
-    'menu-contato': 'Contato'
+    'menu-contato': 'Contato',
+    'home-text':
+      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação.',
+    'about-text':
+      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação. Com 2 anos de experiência prática e uma base sólida obtida ao longo de 2 anos de estudos dedicados, concentro minha expertise em React e Next.js. Sou conhecido pela minha abordagem proativa e comunicativa, sempre buscando soluções inovadoras. Tenho paixão por contribuir em equipes dinâmicas e em projetos desafiadores, onde minha energia positiva e habilidades técnicas impulsionam o sucesso coletivo. Estou ansioso para continuar crescendo e aprendendo em novos ambientes de trabalho!'
   },
-  'en': {
-    'titulo': 'Home',
-    'texto': 'Welcome to the home page in English!',
+  en: {
+    titulo: 'Home',
+    texto: 'Welcome to the home page in English!',
     'menu-pagina-inicial': 'Home',
     'menu-sobre': 'About',
     'menu-projetos': 'Projects',
-    'menu-contato': 'Contact'
+    'menu-contato': 'Contact',
+    'home-text':
+      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation.",
+    'about-text':
+      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation. With 2 years of practical experience and a solid foundation built over 2 years of dedicated studies, I specialize in React and Next.js. I'm known for my proactive and communicative approach, always seeking innovative solutions. I have a passion for contributing to dynamic teams and challenging projects, where my positive energy and technical skills drive collective success. I'm eager to keep growing and learning in new work environments!"
   }
-};
+}
