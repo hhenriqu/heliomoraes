46ba8af8b667d8e99397fdfe615d01c11a02f931
Helio Henrique Moraes <heliohenriquemoraes@gmail.com>
2023-12-19 22:59:38 -0300

    feat: site estruturado e funcionando

--- src/translations.js
+++ src/translations.js
@@ -1,26 +1,41 @@
 const translations = {
   'pt-br': {
-    titulo: 'Página Inicial',
-    texto: 'Bem-vindo à página inicial em português!',
+    'titulo': 'Página Inicial',
+    'texto': 'Bem-vindo à página inicial em português!',
     'menu-pagina-inicial': 'Página inicial',
     'menu-sobre': 'Sobre mim',
     'menu-projetos': 'Projetos',
     'menu-contato': 'Contato',
     'home-text':
-      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação.',
-    'about-text':
-      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação. Com 2 anos de experiência prática e uma base sólida obtida ao longo de 2 anos de estudos dedicados, concentro minha expertise em React e Next.js. Sou conhecido pela minha abordagem proativa e comunicativa, sempre buscando soluções inovadoras. Tenho paixão por contribuir em equipes dinâmicas e em projetos desafiadores, onde minha energia positiva e habilidades técnicas impulsionam o sucesso coletivo. Estou ansioso para continuar crescendo e aprendendo em novos ambientes de trabalho!'
+      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação. 👋',
+    'about-title':
+      'Sobre mim',
+    'about-subtitle':
+      'Olá! Sou Hélio Moraes, um desenvolvedor apaixonado por tecnologia e inovação. 👋',
+    'about-text': 'Com 2 anos de experiência prática e uma base sólida obtida ao longo de 2 anos de estudos dedicados, concentro minha expertise em React e Next.js. Sou conhecido pela minha abordagem proativa e comunicativa, sempre buscando soluções inovadoras. Tenho paixão por contribuir em equipes dinâmicas e em projetos desafiadores, onde minha energia positiva e habilidades técnicas impulsionam o sucesso coletivo. Estou ansioso para continuar crescendo e aprendendo em novos ambientes de trabalho!',
+    'project-title':'Portfólio',
+    'project-subtitle':'Cada projeto é um pedaço unico de desenvolvimento 🧩',
+    'project-code':'Código',
+    'project-live':'Demonstração',
+    'project-tech':'Tecnologias aplicadas',
+    'projectOne-title':'Energisa - Nova Agência',
+    'projectOne-text':'Uma agência digital para uma das maiores empresas de energia do Brasil. O site conta com toda a parte de gestão do usuário(Pagamentos, verificações, adesão e transferências), e também de projetistas (Cadastros, acompanhamentos e aprovaçoes de obras)',
+    'contact-title': 'Contato',
+    'contact-subtitle': 'Não seja tímido! Me chama! 👉',
+      
   },
-  en: {
-    titulo: 'Home',
-    texto: 'Welcome to the home page in English!',
+  'en': {
+    'titulo': 'Home',
+    'texto': 'Welcome to the home page in English!',
     'menu-pagina-inicial': 'Home',
     'menu-sobre': 'About',
     'menu-projetos': 'Projects',
     'menu-contato': 'Contact',
     'home-text':
-      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation.",
+      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation. 👋",
     'about-text':
-      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation. With 2 years of practical experience and a solid foundation built over 2 years of dedicated studies, I specialize in React and Next.js. I'm known for my proactive and communicative approach, always seeking innovative solutions. I have a passion for contributing to dynamic teams and challenging projects, where my positive energy and technical skills drive collective success. I'm eager to keep growing and learning in new work environments!"
+      "Hello! I'm Hélio Moraes, a developer passionate about technology and innovation. With 2 years of practical experience and a solid foundation built over 2 years of dedicated studies, I specialize in React and Next.js. I'm known for my proactive and communicative approach, always seeking innovative solutions. I have a passion for contributing to dynamic teams and challenging projects, where my positive energy and technical skills drive collective success. I'm eager to keep growing and learning in new work environments!",
+      'project-title':'Portfolio',
+    'project-subtitle':'Each project is a unique piece of development 🧩'
   }
 }
